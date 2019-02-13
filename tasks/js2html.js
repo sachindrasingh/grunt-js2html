bb8e02c05746116498fd0aee100da075c7185913
Sachindra Singh <sachindra.singh462@gmail.com>
2019-02-13 15:53:52 +0530

    New feather beautify, angularModule, HTML folder for source files.

--- tasks/js2html.js
+++ tasks/js2html.js
@@ -10,14 +10,16 @@
 
 module.exports = function(grunt) {
     var vm = require('vm'),
-    pretty = require('pretty');
+        pretty = require('pretty'), 
+        fileCount = 0;
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
 
     grunt.registerMultiTask('js2html', 'The best Grunt plugin ever.', function() {
         // Merge task-specific and/or target-specific options with these defaults.
         var options = this.options({
-
+            angularModule: false,
+            beautify: true
         });
 
         // Iterate over all specified file groups.
@@ -25,21 +27,27 @@ module.exports = function(grunt) {
         this.files.forEach(function(f) {
             f.src.forEach(function(filePath) {
                 var content = grunt.file.read(filePath);
-                if(!validateContent(content)){
+                if (!validateContent(content)) {
                     return;
                 }
-                var htmlContent = content.replace(/\\n"[ ]{1,1000}\+/gm, '');
-                htmlContent = htmlContent.replace(/\\"/gm, '"');
-                htmlContent = htmlContent.replace(/\n[ ]{0,1000}"/gm, '');
-                htmlContent = htmlContent.replace(/angular\.module\((.*?)\)/gm, '');
-                htmlContent = htmlContent.replace(/\.run\(\[(.*?),(.*?)function[ ]{0,1000}/gm, '');
-                htmlContent = htmlContent.replace(/\(\$templateCache\)[ ]{0,10}\{[ ]{0,1000}/g, '');
-                htmlContent = htmlContent.replace(/[  \n]\$templateCache\.put\((.*?),/gm, '');
-                htmlContent = htmlContent.replace("\");", '').replace("}]);", "");
-                grunt.file.write(filePath + ".html", pretty(htmlContent));
+                var htmlContent = content.replace(/\\n"[ ]{1,1000}\+/gm, '').replace(/\\"/gm, '"').replace(/\n[ ]{0,1000}"/gm, '');
+                if (options.angularModule) {
+                    htmlContent = htmlContent.replace(/angular\.module\((.*?)\)/gm, '')
+                        .replace(/\.run\(\[(.*?),(.*?)function[ ]{0,1000}/gm, '')
+                        .replace(/\(\$templateCache\)[ ]{0,10}\{[ ]{0,1000}/g, '')
+                        .replace(/[  \n]\$templateCache\.put\((.*?),/gm, '')
+                        .replace("\");", '').replace("}]);", "");
+                }
+                if (options.beautify) {
+                    htmlContent = pretty(htmlContent);
+                }
+                filePath = getDefualtDist(filePath);
+                grunt.file.write(filePath + ".html", htmlContent);
                 grunt.log.writeln('HTML file "' + filePath + '.html" created.');
+                fileCount++;
             })
         });
+        grunt.log.writeln('\nTask complete. Total ' + fileCount + ' html file created.');
     });
 
     function validateContent(content) {
@@ -47,10 +55,17 @@ module.exports = function(grunt) {
         try {
             script = new vm.Script(content);
             return true;
-         } catch (error){
+        } catch (error) {
             grunt.log.writeln(error);
             return false;
-         }
+        }
+    }
+
+    function getDefualtDist(filePath) {
+        filePath = filePath.split('/');
+        filePath.push(filePath[filePath.length - 1]);
+        filePath[filePath.length - 2] = "html";
+        return filePath.join('/');
     }
 
 };
